27594e901a42db0c133fd028b3a793b77b5ad0c0
suhaonan <751550085@qq.com>
2022-05-31 17:48:13 +0800

    修改音频播放问题

--- js/index.js
+++ js/index.js
@@ -7,23 +7,27 @@ window.addEventListener('load',function(){
   var musicBox = document.querySelector('.musicBox');
   var musicAudio = document.querySelector('#musicAudio');
   // 加载页面就开始播放
-  function musicPlay() {
-    musicAudio.autoplay = true;
-    musicAudio.load();
-    musicBox.className = 'musicBox move';
-    document.removeEventListener("touchstart", musicPlay, false);
-  }
-  // 加载页面就开始播放：IOS手机微信端对于音乐的自动播放存在BUG（经常没有声音），我们需要单独的处理一下
-  musicPlay();
-  document.addEventListener("WeixinJSBridgeReady", musicPlay, false);
-  document.addEventListener("touchstart", musicPlay, false);
+  // function musicPlay() {
+  //   window.setTimeout(function(){
+  //     musicAudio.play();
+  //   })
+    
+  //   musicBox.className = 'musicBox move';
+  //   // document.removeEventListener("touchstart", musicPlay, false);
+  // }
+  // // 加载页面就开始播放：IOS手机微信端对于音乐的自动播放存在BUG（经常没有声音），我们需要单独的处理一下
+  // musicPlay();
+  // document.addEventListener("WeixinJSBridgeReady", musicPlay, false);
+  // document.addEventListener("touchstart", musicPlay, false);
 
   // 点击控制播放
   musicBox.onclick = function () {
     // 
     if (musicAudio.paused) {
       // 当前处于暂停状态：控制播放
-      musicPlay();
+      // musicPlay();
+      musicBox.className = 'musicBox move';
+      musicAudio.play();
       return;
     }
     // 当前处于播放状态：控制暂停
